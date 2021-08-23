732d50b098825e446ab84067fa1630c91ce6ab6e
AhmadSaman <ahmad.00079821@gmail.com>
2021-08-23 17:28:35 +0300

    commit tree ba2c46df1ec0c22ea624d8acba58e5ebafc124cc

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,25 @@
-function changeState(state, action) {
-  switch (action.type) {
-    case "counter/increment":
-      return { count: state.count + 1 };
-    default:
-      return state;
-  }
-}
-
 let state = { count: 0 };
+function changeState(state, action) {
+	switch (action.type) {
+		case "counter/increment":
+			return { count: state.count + 1 };
+		default:
+			return state;
+	}
+}
+function dispatch(action) {
+	state = changeState(state, action);
+	render();
+}
+function render() {
+	document.body.textContent = state.count;
+}
 let action = { type: "counter/increment" };
-
+dispatch(action);
+dispatch(action);
+dispatch(action);
+dispatch(action);
 changeState(state, action);
+render();
+dispatch(action);
+dispatch(action);
